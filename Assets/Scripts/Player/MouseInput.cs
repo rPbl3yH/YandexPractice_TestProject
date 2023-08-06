f2c6420f8d886cc84192e0e61de156358c04e87c
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2023-08-06 14:23:59 +0500

    feat: add move controller

---
+++ Assets/Scripts/Player/MouseInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+    public event Action OnMouseDown;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            OnMouseDown?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/MoveController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField] private MouseInput _mouseInput;
+    [SerializeField] private float _upGravityScale = 1f;
+    [SerializeField] private float _downGravityScale = 1f;
+    private Rigidbody2D _rigidbody;
+    private bool _isMoveRequired;
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _mouseInput.OnMouseDown += OnMouseDown;
+    }
+
+    private void OnMouseDown()
+    {
+        _isMoveRequired = true;
+    }
+
+    private void Update()
+    {
+        if (_isMoveRequired)
+        {
+            _rigidbody.gravityScale = -_upGravityScale;
+            _isMoveRequired = false;
+        }
+        else
+        {
+            _rigidbody.gravityScale = _downGravityScale;
+        }
+    }
+}
